2738a67ad33050552599aefd5070789379da6f36
karineferrin <karineferrin@hotmail.fr>
2022-12-06 23:28:07 +0100

    Phase22: codage de la page d'accueil du site

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -72,4 +72,16 @@ class VisiteRepository extends ServiceEntityRepository
                     -> getResult();                   
         }
     }
+     /**
+     * Retourne les n visites les plus récentes
+     * @param type $nb
+     * @retour Visite[]
+     */
+    public function findAllLasted($nb):array{
+        return $this->createQueryBuilder('v') // alias de la table
+           -> orderBy('v.datecreation','DESC')
+           -> setMaxResults($nb)     
+           -> getQuery()
+           -> getResult();
+    }
 }
