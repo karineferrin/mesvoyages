67380bacccb5187e4637b7b10505a4359ba7d70a
karineferrin <karineferrin@hotmail.fr>
2022-12-06 23:56:50 +0100

    Phase24: tests (compléments)

--- tests/VisiteTest.php
+++ tests/VisiteTest.php
@@ -17,4 +17,14 @@ class VisiteTest extends TestCase{
         $visite->setDatecreation(new DateTime("2022-04-14"));
         $this->assertEquals("14/04/2022", $visite->getDatecreationString());
     }
+    public function testAddEnvironnement (){
+        $environnement = new Environnement ();
+        $environnement->setNom("plage");
+        $visite = new visite();
+        $visite -> addEnvironnement($environnement);
+        $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+        $visite -> addEnvironnement ( $environnement);
+        $nbEnvironnementApres = $visite->getEnvironnements()->count ();
+        $this->assertEquals( $nbEnvironnementAvant, $nbEnvironnementApres , "ajouté même environnement devrait échouer");
+    }
 }
